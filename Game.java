b892f292eb811bf17142de5b07def5ef344cb379
Toren G <slickspam4u@gmail.com>
2026-04-03 11:03:28 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -20,84 +20,116 @@ public class Game {
       int y =  (int) (Math.random()*6);
       Player player = new Player(100, "Toren", x, y);
       while (exitGame != true) {
-            System.out.print("Enter command: ");
-            String input = scanner.nextLine().toUpperCase();
-                        
-            if (input.equals("EXIT")) {
-                System.out.println("Exiting game. Goodbye!");
-                exitGame = true;
-            }
-            else if (input.equals("LOOK") || input.equals("SEARCH")) {
-               System.out.println(player.look(dungeonMap));
-            }
-            // TODO: get the player movement working correctly. It should NOT let the player
-            // move beyond the boundaries of the map, and provide the user with descriptions
-            // of what is happening. You'll have to change the if/else/if statements here to 
-            // do this.
-            else if (input.startsWith("MOVE ")) {
-               String direction = input.substring(5);
-               if (direction.equals("UP")) {
-                  if (y+1 > 5) {
-                     y = 5;
-                     System.out.println("Cannot move more this direction");
-                  }
-                  else {
-                     y += 1;
-                     player.moveTo(x,y);
-                     System.out.println(player.getRoom(dungeonMap));
-                  }   
-               } 
-               else if (direction.equals("DOWN")) {
-                  if (y-1 < 0) {
-                     y = 0;
-                     System.out.println("Cannot move more this direction");
-                  }  
-                  else {
-                     y -= 1;
-                     player.moveTo(x,y);
-                     System.out.println(player.getRoom(dungeonMap));
-                  }
-               }
-               else if (direction.equals("LEFT")) {
-                  if (x-1 < 0) {
-                     x = 0;
-                     System.out.println("Cannot move more this direction");
-                  }
-                  else {
-                     x -= 1;
-                     player.moveTo(x,y);
-                     System.out.println(player.getRoom(dungeonMap)); 
-                  }
-               }
-               else if (direction.equals("RIGHT")) {
-                  if (x+1 > 4) {
-                     x = 4;
-                     System.out.println("Cannot move more this direction");
-                  } 
-                  else {
-                     x += 1;
-                     player.moveTo(x,y);
-                     System.out.println(player.getRoom(dungeonMap));
-                  }
+         System.out.print("Enter command: ");
+         String input = scanner.nextLine().toUpperCase();
+                     
+         if (input.equals("EXIT")) {
+            System.out.println("Exiting game. Goodbye!");
+            exitGame = true;
+         }
+         else if (input.equals("LOOK") || input.equals("SEARCH")) {
+            System.out.println(player.look(dungeonMap));
+         }
+         // TODO: get the player movement working correctly. It should NOT let the player
+         // move beyond the boundaries of the map, and provide the user with descriptions
+         // of what is happening. You'll have to change the if/else/if statements here to 
+         // do this.
+         else if (input.startsWith("MOVE ")) {
+            String direction = input.substring(5);
+            if (direction.equals("UP")) {
+               if (y+1 > 5) {
+                  y = 5;
+                  System.out.println("Cannot move more this direction");
                }
                else {
-                  System.out.println("You're trying to move in an invalid direction. Please choose UP, DOWN, LEFT or RIGHT");
+                  y += 1;
+                  player.moveTo(x,y);
+                  System.out.println(player.getRoom(dungeonMap));
+               }   
+            } 
+            else if (direction.equals("DOWN")) {
+               if (y-1 < 0) {
+                  y = 0;
+                  System.out.println("Cannot move more this direction");
+               }  
+               else {
+                  y -= 1;
+                  player.moveTo(x,y);
+                  System.out.println(player.getRoom(dungeonMap));
                }
             }
-            else if (input.startsWith("TAKE ")) {
-               String item = input.substring(5);
-               for (Item i : player.getRoom(dungeonMap).getItems()) {
-                  if (item.equals(i.toString())) {
-                     inventory.addItemToInventory(i);
-                     player.getRoom(dungeonMap).removeItem(i);
-                  }
-                  else {
-                     System.out.println("Item does not exist in room!");
-                  }
+            else if (direction.equals("LEFT")) {
+               if (x-1 < 0) {
+                  x = 0;
+                  System.out.println("Cannot move more this direction");
+               }
+               else {
+                  x -= 1;
+                  player.moveTo(x,y);
+                  System.out.println(player.getRoom(dungeonMap)); 
                }
             }
-         // TODO: display a message to the user telling them their current coordinates every time they move
-         System.out.println("("+x+", "+y+")");
-       }
+            else if (direction.equals("RIGHT")) {
+               if (x+1 > 4) {
+                  x = 4;
+                  System.out.println("Cannot move more this direction");
+               } 
+               else {
+                  x += 1;
+                  player.moveTo(x,y);
+                  System.out.println(player.getRoom(dungeonMap));
+               }
+            }
+            else {
+               System.out.println("You're trying to move in an invalid direction. Please choose UP, DOWN, LEFT or RIGHT");
+            }
+            // TODO: display a message to the user telling them their current coordinates every time they move
+            System.out.println("("+x+", "+y+")");
+         }
+         else if (input.startsWith("TAKE ")) {
+            String item = input.substring(5);
+            boolean itemFound = false;
+
+            for (Item i : player.getRoom(dungeonMap).getItems()) {
+               if (item.equalsIgnoreCase(i.toString())) {
+                  inventory.addItemToInventory(i);
+                  player.getRoom(dungeonMap).removeItem(i);
+                  System.out.println("You took the " + i.toString() + ".");
+                  itemFound = true;
+                  break;
+               }
+            }
+
+            if (!itemFound) {
+               System.out.println("Item does not exist in room!");
+            }
+         }
+      else if (input.startsWith("DROP ")) {
+         String item = input.substring(5);
+         Item itemToDrop = null;
+    
+         for (Item i : inventory.getItems()) {
+             if (item.equalsIgnoreCase(i.toString())) {
+                 itemToDrop = i;
+                 break;
+             }
+         }
+    
+         if (itemToDrop != null) {
+            inventory.removeItemFromInventory(itemToDrop);
+            player.getRoom(dungeonMap).addItem(itemToDrop);
+            System.out.println("Dropped "+itemToDrop.toString()+".");
+         } 
+         else {
+            System.out.println("Item does not exist in inventory!");
+         }
+         } 
+         /*else if (input.startsWith("FIGHT ")) {
+            String monster = input.substring(5);
+            boolean existsInRoom = false;
+            for (Monster m : player.getRoom(dungeonMap).getMonsters()) {
+            }
+         }*/
+      }
    }
 }
